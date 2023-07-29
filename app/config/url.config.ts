13049fd0f212c96971701e79e9b668e12c2634d6
Vadim <thekinv21@gmail.com>
2023-07-30 01:27:09 +0300

    FEAR : create urls config

---
+++ app/config/url.config.ts
@@ -0,0 +1,8 @@
+// bunları neden yazdık?
+// bunları yazmamızın sebebi şu ki bize map işlemi yaparken url döndürüsün
+
+export const getGenreUrl = (slug: string) => `/genre/${slug}`
+export const getMovieUrl = (slug: string) => `/movie/${slug}`
+export const getActorUrl = (slug: string) => `/actor/${slug}`
+export const getAdminUrl = (url: string) => `/manage/${url}`
+export const getAdminHomeUrl = () => getAdminUrl('').slice(0, -1)
