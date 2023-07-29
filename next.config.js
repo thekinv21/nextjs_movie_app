6899880917765ed16eb7f4c9d87ee787bc65d8e7
Vadim <thekinv21@gmail.com>
2023-07-29 21:57:55 +0300

    FEAR : CREATE NEXT-CONFIG

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true, // strict code , warning
+	poweredByHeader: false, // herhangi bir uygulama ile yapılmış oldğunu gösterme
+	optimizeFonts: false,
+
+	//  env file : gizli bilgiler
+	env: {
+		APP_URL: process.env.REACT_APP_URL,
+		APP_ENV: process.env.REACT_APP_ENV,
+		APP_SERVER_URL: process.env.REACT_APP_SERVER_URL
+	},
+
+	// bunu neden yazdık ?
+
+	// frontendeki herhangi bir api değil sadece backendeki api açılmasını sağlar
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: 'http://localhost:4200/api/:path*'
+			},
+			{
+				source: '/uploads/:path*',
+				destination: 'http://localhost:4200/uploads/:path*'
+			}
+		]
+	}
 }
 
 module.exports = nextConfig
